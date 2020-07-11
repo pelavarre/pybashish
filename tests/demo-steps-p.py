587584ea833f56e6b62ee2af7061d251f2b26afb
Pat LaVarre <plavarre@purestorage.com>
2020-07-11 15:23:57 -0700

    fixup! Tweak up the 2m video demo scripts

---
+++ tests/demo-steps-p.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+usage: p.py [-h] HELLO
+
+do good stuff
+
+positional arguments:
+  HELLO          an arg for us
+
+optional arguments:
+  -h, --help     show this help message and exit
+
+examples:
+  p.py yah
+"""
+
+import argdoc
+
+
+def main():
+    args = argdoc.parse_args()
+    main.args = args
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
