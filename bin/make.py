55a816101fa27874b112f000db57846d1e87b23d
Pat LaVarre <plavarre@purestorage.com>
2020-07-21 12:31:57 -0700

    fixup! fixup! fixup! Tweak up the 2m video demo scripts

---
+++ bin/make.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+usage: make.py [-h]
+
+figure out how to make more things from some things
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+bugs:
+  don't understand most of what the real "make" knows
+
+examples:
+  Oh no! No examples disclosed!! 💥 💔 💥
+"""
+
+import argdoc
+
+
+def main():
+    args = argdoc.parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# copied from:  git clone https://github.com/pelavarre/pybashish.git
