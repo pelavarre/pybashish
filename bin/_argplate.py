ffc19b6162106a96fa8fc0d30b273e603fd5cdf8
Pat LaVarre <plavarre@purestorage.com>
2021-07-23 19:42:51 -0700

    first commit of:  bin/_argplate.py
    to show how to include examples in argparse help lines

---
+++ bin/_argplate.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+r"""
+usage: _argplate.py [-h]
+
+show how to include examples in argparse help lines
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+examples:
+  _argplate.py -h  # show this help message and exit
+"""
+
+# code reviewed by people, and by Black and Flake8 bots
+
+
+import __main__
+import argparse
+import difflib
+import os
+import sys
+
+
+def main(argv):
+    """Run from the command line"""
+
+    parser = compile_argdoc(epilog="examples:")
+    args = parser.parse_args(argv[1:])
+    print(args)
+
+
+# deffed in many files  # missing from docs.python.org
+def compile_argdoc(epilog):
+    """Declare how to parse the command line"""
+
+    doc = __main__.__doc__
+    prog = doc.strip().splitlines()[0].split()[1]
+    description = list(_ for _ in doc.strip().splitlines() if _)[1]
+    epilog_at = doc.index(epilog)
+    epilog = doc[epilog_at:]
+
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description=description,
+        add_help=True,
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=epilog,
+    )
+
+    exit_unless_main_doc_eq(parser)
+    return parser
+
+
+# deffed in many files  # missing from docs.python.org
+def exit_unless_main_doc_eq(parser):
+    """Exit nonzero, unless __main__.__doc__ equals "parser.format_help()" """
+
+    file_filename = os.path.split(__file__)[-1]
+
+    main_doc = __main__.__doc__.strip()
+    parser_doc = parser.format_help()
+
+    got = main_doc
+    got_filename = "./{} --help".format(file_filename)
+    want = parser_doc
+    want_filename = "argparse.ArgumentParser(..."
+
+    diff_lines = list(
+        difflib.unified_diff(
+            a=got.splitlines(),
+            b=want.splitlines(),
+            fromfile=got_filename,
+            tofile=want_filename,
+        )
+    )
+
+    if diff_lines:
+
+        lines = list((_.rstrip() if _.endswith("\n") else _) for _ in diff_lines)
+        print("\n".join(lines))
+
+        print("error: update main argdoc to match help, or vice versa")
+
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+# copied from:  git clone https://github.com/pelavarre/pybashish.git
