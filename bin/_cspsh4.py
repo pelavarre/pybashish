33dc811457c8641af4840a0784fce6a01fe47685
Pat LaVarre <plavarre@purestorage.com>
2021-07-27 20:11:49 -0700

    cspsh:  Factor CspTree, CspLeaf, CspTuple out of the first 8 instances

--- bin/_cspsh4.py
+++ bin/_cspsh4.py
@@ -34,15 +34,59 @@ import sys
 import textwrap
 
 
+#
+# Think in Lisp, while coding in Python
+#
+
+
 DENT = 4 * " "
 
 
-class AfterProc(collections.namedtuple("AfterProc", "before after".split())):
+class CspTree:
     def _to_deep_py_(self, depth):
+
         pys = list()
-        pys.append("AfterProc(")
-        pys.append(DENT + "before=" + self.before._to_deep_py_(depth + 1) + ",")
-        pys.append(DENT + "after=" + self.after._to_deep_py_(depth + 1) + ",")
+        pys.append(type(self).__name__ + "(")
+
+        for field in self._fields:
+            tree = getattr(self, field)
+            if not hasattr(tree, "_to_deep_py_"):
+                pys.append(DENT + field + '="{}",'.format(tree))
+            else:
+                pys.append(DENT + field + "=" + tree._to_deep_py_(depth + 1) + ",")
+
+        pys.append(")")
+
+        dent = depth * DENT
+        py = ("\n" + dent).join(pys)
+
+        return py
+
+        # .format(... tree ...) goes wrong over quotes, line breaks, etc
+
+
+class CspLeaf(CspTree):
+    def _to_deep_py_(self, depth):
+
+        assert len(self._fields) == 1, self._fields
+
+        field = self._fields[-1]
+        tree = getattr(self, field)
+        py = '{}("{}")'.format(type(self).__name__, tree)
+
+        return py
+
+        # .format(... tree ...) goes wrong over quotes, line breaks, etc
+
+
+class CspList(CspTree):
+    def _to_deep_py_(self, depth):
+
+        pys = list()
+        pys.append(type(self).__name__ + "(")
+
+        for item in self:
+            pys.append(DENT + item._to_deep_py_(depth + 1) + ",")
         pys.append(")")
 
         dent = depth * DENT
@@ -50,84 +94,50 @@ class AfterProc(collections.namedtuple("AfterProc", "before after".split())):
         return py
 
 
-class EventsProc(collections.namedtuple("EventsProc", "name events body".split())):
-    def _to_deep_py_(self, depth):
-        pys = list()
-        pys.append("EventsProc(")
-        pys.append(DENT + 'name="{}",'.format(self.name))
-        pys.append(DENT + "events=" + self.events._to_deep_py_(depth + 1) + ",")
-        pys.append(DENT + "body=" + self.body._to_deep_py_(depth + 1) + ",")
-        pys.append(")")
-
-        dent = depth * DENT
-        py = ("\n" + dent).join(pys)
-        return py
+def to_deep_py(csp_tree):
+    py = csp_tree._to_deep_py_(0)
+    return py
 
 
-class ChoiceTuple(tuple):
+#
+# Declare a CSP Lisp
+#
+
+
+class AfterProc(collections.namedtuple("AfterProc", "before after".split()), CspTree):
+    pass
+
+
+class EventsProc(
+    collections.namedtuple("EventsProc", "name events body".split()), CspTree
+):
+    pass
+
+
+class ChoiceTuple(tuple, CspList):
     def __new__(cls, *args):
         return super().__new__(cls, args)
 
-    def _to_deep_py_(self, depth):
-        pys = list()
-        pys.append("ChoiceTuple(")
-        for event in self:
-            pys.append(DENT + event._to_deep_py_(depth + 1) + ",")
-        pys.append(")")
 
-        dent = depth * DENT
-        py = ("\n" + dent).join(pys)
-        return py
-
-
-class Event(collections.namedtuple("Event", "name".split())):
-    def _to_deep_py_(self, depth):
-        py = 'Event("{}")'.format(self.name)  # wrong over quotes or line breaks
-        return py
+class Event(CspLeaf, collections.namedtuple("Event", "name".split())):
+    pass
 
 
 class EventPair(collections.namedtuple("EventPair", "head tail".split())):
     pass
 
 
-class EventTuple(tuple):
+class EventTuple(tuple, CspList):
     def __new__(cls, *args):
         return super().__new__(cls, args)
 
-    def _to_deep_py_(self, depth):
-        pys = list()
-        pys.append("EventTuple(")
-        for event in self:
-            pys.append(DENT + event._to_deep_py_(depth + 1) + ",")
-        pys.append(")")
 
-        dent = depth * DENT
-        py = ("\n" + dent).join(pys)
-        return py
+class ProcDef(collections.namedtuple("ProcDef", "name body".split()), CspTree):
+    pass
 
 
-class ProcDef(collections.namedtuple("ProcDef", "name body".split())):
-    def _to_deep_py_(self, depth):
-        pys = list()
-        pys.append("ProcDef(")
-        pys.append(DENT + 'name="{}",'.format(self.name))
-        pys.append(DENT + "body=" + self.body._to_deep_py_(depth + 1) + ",")
-        pys.append(")")
-
-        dent = depth * DENT
-        py = ("\n" + dent).join(pys)
-        return py
-
-
-class DeffedProc(collections.namedtuple("DeffedProc", "name".split())):
-    def _to_deep_py_(self, depth):
-        py = 'DeffedProc("{}")'.format(self.name)  # wrong over quotes or line breaks
-        return py
-
-
-def to_deep_py(csp_tree):  # TODO: derive "type(csp_tree)" from "collections.namedtuple"
-    py = csp_tree._to_deep_py_(0)
-    return py
+class DeffedProc(collections.namedtuple("DeffedProc", "name".split()), CspLeaf):
+    pass
 
 
 #
@@ -241,6 +251,17 @@ def try_to_deep_py():
         """
     ).strip()
 
+    # choc → X  # event guards process
+
+    want11 = textwrap.dedent(
+        """
+        AfterProc(
+            before=Event("choc"),
+            after=DeffedProc("X"),
+        )
+        """
+    ).strip()
+
     # coin → (choc → X | toffee → X)  # events guard processes
 
     want2 = textwrap.dedent(
@@ -292,7 +313,7 @@ def try_to_deep_py():
         """
     ).strip()
 
-    wants = (want0, want1, want2, want3)
+    wants = (want0, want1, want11, want2, want3)
     for want in wants:
         csp_tree = eval(want)
         py = to_deep_py(csp_tree)
@@ -321,7 +342,7 @@ def stderr_print_diff(**kwargs):
     )
 
     if diff_lines:
-        stderr_print(diff_lines)
+        stderr_print("\n".join(diff_lines))
 
     return diff_lines
 
