f1e1951fef6e2d9fdd3214abea0aa78dc5679b67
Pat LaVarre <plavarre@purestorage.com>
2021-12-18 22:47:46 -0800

    cp:  Solve the 'cp.py' test cases

--- bin/cp.py
+++ bin/cp.py
@@ -6,37 +6,30 @@ usage: cp.py [-h] [-i] [-p] [-R] [FILE]
 duplicate a file (make it found twice)
 
 positional arguments:
-  FILE        the file to duplicate (default: last modified in cwd)
+  FILE        the file to duplicate (default: last modified in cwd, else '/dev/null')
 
 optional arguments:
   -h, --help  show this help message and exit
   -i          ask before replacing some other file
   -p          copy the permissions of the file too, not just its bytes
-  -R          copy the dirs and files inside a dir too, don't just give up on it
+  -R          copy dirs (and the dirs and files inside the dirs)
 
 quirks:
-  chooses a new name for you, when none provided, unlike bash "cp"
-  increments the name, such as:  touch it~41~ && touch it && cp.py  # it~41~ it it~42~
+  chooses new file names like Emacs would:  'null', 'null~', 'null~2~', ...
+  copies without changing the name, when the Cwd doesn't contain the name
+  copies from hostname:path on request, but defaults to copy from Localhost
+  copies always in the way of 'cp -ipR'
 
 examples:
   cp.py  # backs up last modified file of cwd (makes it found twice)
-  cp.py -  # creates copy of Stdin named "stdin~1~", then "stdin~2~", etc
-  cp.py /dev/null  # creates empty file named "null~1~", etc
-  cp.py itself  # creates backup named "itself~1~", etc
+  cp.py -  # creates copy of Stdin named "stdin~", then "stdin~2~", etc
+  cp.py localhost:/etc/passwd  # backs up a file from a remote host on request
 """
-
-# FIXME: mmm Emacs takes "it~1~" as a read only path
-
-# FIXME: except cp.py from outside this dir should mean cp -ip to this dir
-# FIXME: also copy from (FILE | HOSTNAME:FILE) to here, like Bash "scp" would
-# FIXME: think about "cp SOURCE TARGET" vs "cp TARGET SOURCE" vs line-editor's
-
-# FIXME: somehow define a way to default to "cp -ipR"
-
-# FIXME: spec & test the two arg case of cp: '...' and '...' are the same file
 
 
 import glob
+import os
+import re
 import shlex
 import subprocess
 import sys
@@ -48,35 +41,127 @@ def main():
 
     args = argdoc.parse_args()
 
-    if not sys.argv[1:]:
+    # Pick a FromPath to copy from
+    frompath = os_path_choose() if (args.file is None) else args.file
 
-        inc_filename()
+    # Pick a ToName to copy to
 
-        sys.exit()
+    (dirname, basename) = os.path.split(frompath)
+    (base, ext, rev) = os_path_partition(basename)
+    toname = os_path_nextname(basename=base)
 
-    sys.stderr.write("{}\n".format(args))
-    sys.stderr.write("{}\n".format(argdoc.format_usage().rstrip()))
-    sys.stderr.write("cp.py: error: not implemented\n")
+    # Copy and touch
 
-    sys.exit(2)  # exit 2 from rejecting usage
+    cp_shline = "cp -ipR {} {}".format(frompath, toname)
+    if frompath == "/dev/null":
+        cp_shline = "cp -i {} {}".format(frompath, toname)
+    elif ":" in frompath:
+        cp_shline = "scp -pqr {} {}".format(frompath, toname)
+
+    touch_shline = "touch {}".format(toname)
+
+    for shline in (cp_shline, touch_shline):
+
+        shargv = shlex.split(shline)
+        sys.stderr.write("+ {}\n".format(shline))
+        subprocess.run(shargv, stdin=subprocess.PIPE, check=True)
 
 
-def inc_filename():
+# deffed in many files  # missing from docs.python.org
+def os_path_choose():
+    """Find the last modified, else highest name, else '/dev/null' in the Cwd"""
 
-    paths = sorted(glob.glob("it*"))  # FIXME: wrong to sort ~10~ before ~9~
-    assert paths
+    paths = os.listdir()
+    paths.sort(key=lambda _: os.stat(_).st_mtime)
 
+    if not paths:
+
+        return "/dev/null"
+
+    path = paths[-1]
+
+    return path
+
+
+# deffed in many files  # missing from docs.python.org
+def os_path_intrev(path):
+    """Pick the Rev Int out of a Path"""
+
+    (base, ext, rev) = os_path_partition(path)
+    intrev = os_path_rev_eval(rev, default=0)
+
+    return intrev
+
+
+# deffed in many files  # missing from docs.python.org
+def os_path_nextname(basename):
+    """Pick the next Filename not already existing in the Cwd"""
+
+    pattern = basename + "*"
+
+    paths = list(glob.glob(pattern))
+
+    last_path = basename
+    if not paths:
+
+        return basename
+
+    paths.sort(key=lambda _: os_path_intrev(_))
     last_path = paths[-1]
-    revision = int(last_path.split("~")[-2])
 
-    next_path = "it~{}~".format(revision + 1)
+    (base, ext, rev) = os_path_partition(last_path)
+    last_intrev = os_path_intrev(last_path)
+    next_intrev = last_intrev + 1
 
-    shline = "cp -ipR {} {}".format(last_path, next_path)
-    shargv = shlex.split(shline)
-    sys.stderr.write("+ {}\n".format(shline))
-    subprocess.run(shargv, stdin=subprocess.PIPE, check=True)
+    next_path = "{}{}~".format(base, ext)
+    if next_intrev != 1:
+        next_path = "{}{}~{}~".format(base, ext, next_intrev)
 
-    print(next_path)
+    return next_path
+
+
+# deffed in many files  # missing from docs.python.org
+def os_path_partition(path):
+    """Pick apart the Base, the Ext, and the Rev"""
+
+    (dirname, basename) = os.path.split(path)
+    (base_plus, ext_plus) = os.path.splitext(basename)
+
+    base = base_plus
+    ext = ext_plus
+    rev = ""
+
+    if "~" in ext_plus:
+
+        splits = ext_plus.split("~")
+        ext = splits[0]
+
+        rev = "~" + "~".join(splits[1:])
+
+    elif (not ext_plus) and ("~" in base_plus):
+
+        splits = base_plus.split("~")
+        base = splits[0]
+
+        rev = "~" + "~".join(splits[1:])
+
+    assert (base + ext + rev) == basename, (base, ext, rev, path)
+
+    return (os.path.join(dirname, base), ext, rev)
+
+
+# deffed in many files  # missing from docs.python.org
+def os_path_rev_eval(rev, default):
+    """Pick the Rev Int out of the Rev Str"""
+
+    int_rev = default
+    if "~" in rev:
+        int_rev = 1
+        match = re.match(r"^~([0-9]+)~?$", string=rev)
+        if match:
+            int_rev = int(match.group(1))
+
+    return int_rev  # such as 7 from "it~7~" or from "it~7"
 
 
 if __name__ == "__main__":
